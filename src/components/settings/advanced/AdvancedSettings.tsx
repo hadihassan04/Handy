e0200a4770f43a4ff8a5f4b7fb4d67d612540901
Hadi Hassan <hadihassan04@gmail.com>
2026-09-25 15:44:35 +0200

    feat: add Menu Bar Only toggle to Advanced settings
    Surfaces the new menu_bar_only setting as a macOS-only toggle in
    Advanced settings, next to Show Tray Icon.

--- src/components/settings/advanced/AdvancedSettings.tsx
+++ src/components/settings/advanced/AdvancedSettings.tsx
@@ -7,6 +7,7 @@ import { SettingsGroup } from "../../ui/SettingsGroup";
 import { StartHidden } from "../StartHidden";
 import { AutostartToggle } from "../AutostartToggle";
 import { ShowTrayIcon } from "../ShowTrayIcon";
+import { MenuBarOnly } from "../MenuBarOnly";
 import { PasteMethodSetting } from "../PasteMethod";
 import { TypingToolSetting } from "../TypingTool";
 import { ClipboardHandlingSetting } from "../ClipboardHandling";
@@ -35,6 +36,7 @@ export const AdvancedSettings: React.FC = () => {
         <StartHidden descriptionMode="tooltip" grouped={true} />
         <AutostartToggle descriptionMode="tooltip" grouped={true} />
         <ShowTrayIcon descriptionMode="tooltip" grouped={true} />
+        <MenuBarOnly descriptionMode="tooltip" grouped={true} />
         <ShowOverlay descriptionMode="tooltip" grouped={true} />
         <ModelUnloadTimeoutSetting descriptionMode="tooltip" grouped={true} />
         <ExperimentalToggle descriptionMode="tooltip" grouped={true} />
